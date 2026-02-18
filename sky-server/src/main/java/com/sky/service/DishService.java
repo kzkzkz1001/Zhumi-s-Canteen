fe683f7b267031501ad06e6fd2b5fbb39d419571
kezhen <kezhen123zhang@gmail.com>
2026-02-18 21:13:55 +0000

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,10 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 public class DishService {
     /**
@@ -9,5 +13,21 @@ public class DishService {
      */
     public void save(DishDTO dishDTO){
 
+    }
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        return null;
+    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     */
+    public void deleteBatch(List<Long> ids) {
     }
 }
