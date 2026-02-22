747dcaa7c7a802407a077fb8341033b0fa54e913
kezhen <kezhen123zhang@gmail.com>
2026-02-22 18:53:00 +0000

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -66,4 +66,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    /**
+     * 显示购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
+
+
 }
